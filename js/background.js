ca8d985232a2e0f04e1bc01eab9baabbdaec3afc
chabin <chabin@skbkontur.ru>
2018-07-16 10:57:25 +0500

    Focus tab with figma and close

--- js/background.js
+++ js/background.js
@@ -1,7 +1,9 @@
-var storage = chrome.storage.local;
-var browserAction = chrome.browserAction;
+var storage = chrome.storage.local,
+    browserAction = chrome.browserAction,
+    windows = chrome.windows,
+    tabs = chrome.tabs;
 
-storage.get('enabled', function(items) {
+storage.get('enabled', function (items) {
     isEnabled = items.enabled;
     if (isEnabled || isEnabled == undefined)
         browserAction.setIcon({ 'path': './icons/icon-32.png' });
@@ -9,9 +11,29 @@ storage.get('enabled', function(items) {
         browserAction.setIcon({ 'path': './icons/icon-32-disabled.png' });
 });
 
-chrome.extension.onMessage.addListener(function(request) {
+chrome.extension.onMessage.addListener(function (request) {
     if (request == "enabled")
         browserAction.setIcon({ 'path': './icons/icon-32.png' });
     else if (request == "disabled")
         browserAction.setIcon({ 'path': './icons/icon-32-disabled.png' });
 });
+
+tabs.onCreated.addListener(function (createdTab) {
+    tabs.query({ windowId: windows.WINDOW_ID_CURRENT }, function (openTabs) {
+        openTabs.forEach(tab => {
+            if (tab.id === createdTab.id) {
+                let url = tab.url,
+                    // Files
+                    figmaFileURL = 'https://www.figma.com/file',
+                    isFigmaFile = url.indexOf(figmaFileURL) == 0,
+                    // Protos
+                    figmaProtoURL = 'https://www.figma.com/proto',
+                    isFigmaProto = url.indexOf(figmaProtoURL) == 0;
+
+                if (isFigmaFile || isFigmaProto) {
+                    tabs.update(tab.id, { active: true });
+                }
+            }
+        });
+    });
+})
